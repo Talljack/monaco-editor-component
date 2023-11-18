0f26db1be895228c31b9347a786a583e2e82c33f
Yugang Cao <1442770751@qq.com>
2023-11-18 14:49:49 +0800

    fix: demo (#6)

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,21 +1,6 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
-
-const theme = extendTheme({ colors })
-
-ReactDOM.render(
-  <ChakraProvider theme={theme}>
-    <App />
-  </ChakraProvider>,
-  document.getElementById('root'),
-)
+const rootElement = document.getElementById('root')
+createRoot(rootElement!).render(<App />)
