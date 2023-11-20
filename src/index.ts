382cc84cd98b1bbeb902b701c2a97baeab91e70f
Yugang Cao <1442770751@qq.com>
2023-11-20 21:30:07 +0800

    chore: vue and react

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './react'
+export * from './type'
 import { MonacoDiffEditor as VMonacoDiffEditor, MonacoEditor as VMonacoEditor, monaco } from './vue'
 
 export { VMonacoDiffEditor, VMonacoEditor, monaco }
