8378a49dffaa6e13812a6c113d8c8a35437a8722
yugang.cao <yugang.cao@tusen.ai>
2023-12-07 11:04:05 +0800

    fix: update test demo

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,17 @@
 import { describe, expect, it } from 'vitest'
 import { formatWidth } from '../src/utils'
 
-describe('utils', () => {
-  it('formatWidth', () => {
-    it('percent', () => {
-      expect(formatWidth('50%')).toBe('50%')
-    })
-    it('number', () => {
-      expect(formatWidth(50)).toBe('50px')
-    })
-    it('string', () => {
-      expect(formatWidth('50px')).toBe('50px')
-    })
-    it('undefined', () => {
-      expect(formatWidth(undefined)).toBe('100%')
-    })
+describe('utils/formatWidth', () => {
+  it('percent', () => {
+    expect(formatWidth('50%')).toBe('50%')
+  })
+  it('number', () => {
+    expect(formatWidth(50)).toBe('50px')
+  })
+  it('string', () => {
+    expect(formatWidth('50px')).toBe('50px')
+  })
+  it('undefined', () => {
+    expect(formatWidth(undefined)).toBe('100%')
   })
 })
