bf3c750ab33f894cb55be513dab89911ace7c304
Yugang Cao <1442770751@qq.com>
2024-01-20 19:38:52 +0800

    chore: update deps (#103)
    * feat: deps

--- demoPkg/react/vite.config.ts
+++ demoPkg/react/vite.config.ts
@@ -1,10 +1,10 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     watch: {
       usePolling: true,
